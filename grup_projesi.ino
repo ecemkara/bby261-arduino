7fc28c460bea3dfdd1f6111ad6b9503b30228aba
ecemkara <56671724+ecemkara@users.noreply.github.com>
2019-12-19 09:43:07 +0300

    Add files via upload

--- grup_projesi.ino
+++ grup_projesi.ino
@@ -1,63 +1,68 @@
+#include <LiquidCrystal.h>
 
-#include <Wire.h>
-
-int red =8;
-int green= 9;
-
-int buzzerPin=7;     
-
-void setup() {
-  pinMode(red,OUTPUT);
-  pinMode(green,OUTPUT);
-  Serial.begin(9600);
-  pinMode(buzzerPin,OUTPUT); 
-}
+int lm35Pin = A4 ; 
+int red= 10; 
+int green = 11; 
+int yellow = 12;
 
 
-double Termistor(int analogOkuma){
+#define buzzer 13 
 
- double sicaklik;
- sicaklik = log(((10240000 / analogOkuma) - 10000));
- sicaklik = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * sicaklik * sicaklik)) * sicaklik);
- sicaklik = sicaklik - 273.15;
- return sicaklik;
-}
-
-
-void loop() {
-               
+ int zaman = 50; 
+ int okunan_deger = 0; 
+ float sicaklik_gerilim = 0; 
+ float sicaklik = 0; 
  
-  int deger = analogRead(A0);
-    
-  double sicaklik = Termistor(deger);
-   
-  Serial.println(sicaklik);
-
-  //sıcaklık 5 derece üstünde ise kırmızı led yansın ve buzzer ötsün
-  
-  if(sicaklik > 5){
-    
-    digitalWrite(buzzerPin,HIGH); 
-    digitalWrite(red,HIGH);
-    digitalWrite(green,LOW);
-    
-    delay(1000); 
-  }
-  
-  // diğer durumda yeşil led yansın kırmızı led sönsün
-  
-  else{
-     digitalWrite(buzzerPin,LOW); 
-    digitalWrite(green,HIGH);
-    digitalWrite(red,LOW);
-    
-  }
-
-    delay(250);
-
-
+ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
  
-   
+ void setup()  
+ { 
+   lcd.begin(16, 2);
+   lcd.print("Sicaklik Olcumu:");
+  pinMode(red,OUTPUT); 
+  pinMode(green,OUTPUT); 
   
+   pinMode(buzzer,OUTPUT); 
+    pinMode(yellow,OUTPUT); 
+   Serial.begin(9600); 
+ } 
+   
+ void loop() 
+ { 
+   okunan_deger = analogRead(lm35Pin); 
+   sicaklik_gerilim = (okunan_deger / 1023.0)*5000.0; 
+   sicaklik = sicaklik_gerilim /10.0;  
+   Serial.println(sicaklik);     
+    lcd.setCursor(0, 1);
+  lcd.print(sicaklik);
+  lcd.setCursor(6,1);
+  lcd.print("C");
+  delay(100);
 
-}
+  if(0< sicaklik <= 25){ 
+     digitalWrite(red,LOW); 
+     digitalWrite(buzzer,LOW);
+      digitalWrite(green,HIGH);  
+           digitalWrite(yellow,LOW); 
+      
+     delay(100); 
+   } 
+   if(25 < sicaklik < 35){ 
+     digitalWrite(red,LOW); 
+     digitalWrite(buzzer,LOW);
+      digitalWrite(green,LOW);  
+           digitalWrite(yellow,HIGH); 
+      
+     delay(100); 
+   } 
+   if(sicaklik > 35){ 
+ 
+
+    digitalWrite(green,LOW); 
+     digitalWrite(buzzer,HIGH); 
+      digitalWrite(red,HIGH); 
+         digitalWrite(yellow,LOW); 
+     delay(100); 
+      
+   } 
+ } 
